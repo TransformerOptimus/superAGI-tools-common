d3f6aa1417113ab0229dbf982db83830b7c886b6
abhijeet <abhijeet@contlo.com>
2023-08-16 13:49:40 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Superagi Tools
+
+This is a Python toolkit for AI tools provided by Superagi
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='superagi_tools',
+    version='1.0.6',
+    description='A useful module for using tools from Superagi',
+    author='superagi',
+    author_email='mukunda@superagi.com',
+    packages=find_packages(),
+    install_requires=['pydantic==1.10.8', 'PyYAML==6.0'],
+)
